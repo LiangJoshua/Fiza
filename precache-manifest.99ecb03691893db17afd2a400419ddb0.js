460a5a199bbe4f3fa4c1fbc9bc2b240ce4e5865d
Joshua Liang <joshuali@zillowgroup.com>
2020-05-22 00:06:47 -0700

    Updates

---
+++ precache-manifest.99ecb03691893db17afd2a400419ddb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67c86f016f25f4fc06590b99de4b2a8",
+    "url": "/fiza/index.html"
+  },
+  {
+    "revision": "926c257102cda7bd2c8d",
+    "url": "/fiza/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "02d464ed094d50e30465",
+    "url": "/fiza/static/js/2.c4722e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fiza/static/js/2.c4722e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926c257102cda7bd2c8d",
+    "url": "/fiza/static/js/main.907cd5b8.chunk.js"
+  },
+  {
+    "revision": "940b4eded458eff01f0a",
+    "url": "/fiza/static/js/runtime-main.a4884bb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fiza/static/media/logo.5d5d9eef.svg"
+  }
+]);
